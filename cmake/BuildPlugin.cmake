b5db08059be62dc39d4f986603d225c3574b68d6
Alexey Vyskubov <alexey@ocaml.nl>
2025-02-27 14:43:46 +0200

    cleanup

--- cmake/BuildPlugin.cmake
+++ cmake/BuildPlugin.cmake
@@ -1,6 +1,5 @@
 MACRO (BUILD_LXQT_PLUGIN NAME)
     set(PROGRAM "lxqt-panel")
-    project(${PROGRAM}_${NAME})
 
     set(PROG_SHARE_DIR ${CMAKE_INSTALL_FULL_DATAROOTDIR}/lxqt/${PROGRAM})
     set(PLUGIN_SHARE_DIR ${PROG_SHARE_DIR}/${NAME})
@@ -10,7 +9,7 @@ MACRO (BUILD_LXQT_PLUGIN NAME)
     set(QTX_LIBRARIES Qt6::Widgets)
     set(SRC ${HEADERS} ${SOURCES})
     add_library(${NAME} MODULE ${SRC}) # build dynamically loadable modules
-    target_link_libraries(${NAME} ${QTX_LIBRARIES} lxqt KF6::WindowSystem)
+    target_link_libraries(${NAME} ${QTX_LIBRARIES} lxqt)
 
     install(TARGETS ${NAME} DESTINATION ${PLUGIN_DIR})
     install(FILES ${CONFIG_FILES}  DESTINATION ${PLUGIN_SHARE_DIR})
