53221997b5c1e21b0453ac90cec480851bd4e559
Alexey Vyskubov <alexey@ocaml.nl>
2025-02-27 14:39:12 +0200

    cleanup

--- cmake/BuildPlugin.cmake
+++ cmake/BuildPlugin.cmake
@@ -7,28 +7,12 @@ MACRO (BUILD_LXQT_PLUGIN NAME)
 
     file (GLOB CONFIG_FILES resources/*.conf)
 
-    if (NOT DEFINED PLUGIN_DIR)
-        set (PLUGIN_DIR ${CMAKE_INSTALL_FULL_LIBDIR}/${PROGRAM})
-    endif (NOT DEFINED PLUGIN_DIR)
-
     set(QTX_LIBRARIES Qt6::Widgets)
-    if(QT_USE_QTXML)
-        set(QTX_LIBRARIES ${QTX_LIBRARIES} Qt6::Xml)
-    endif()
-    if(QT_USE_QTDBUS)
-        set(QTX_LIBRARIES ${QTX_LIBRARIES} Qt6::DBus)
-    endif()
-
-    list(FIND STATIC_PLUGINS ${NAME} IS_STATIC)
     set(SRC ${HEADERS} ${SOURCES})
-    if (${IS_STATIC} EQUAL -1) # not static
-        add_library(${NAME} MODULE ${SRC}) # build dynamically loadable modules
-        install(TARGETS ${NAME} DESTINATION ${PLUGIN_DIR}) # install the *.so file
-    else() # static
-        add_library(${NAME} STATIC ${SRC}) # build statically linked lib
-    endif()
-    target_link_libraries(${NAME} ${QTX_LIBRARIES} lxqt ${LIBRARIES} KF6::WindowSystem)
+    add_library(${NAME} MODULE ${SRC}) # build dynamically loadable modules
+    target_link_libraries(${NAME} ${QTX_LIBRARIES} lxqt KF6::WindowSystem)
 
+    install(TARGETS ${NAME} DESTINATION ${PLUGIN_DIR})
     install(FILES ${CONFIG_FILES}  DESTINATION ${PLUGIN_SHARE_DIR})
 
 ENDMACRO(BUILD_LXQT_PLUGIN)
